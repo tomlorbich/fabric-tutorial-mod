b23a7393aa790b380d921d21c44d435f25edf5a7
tommy <tommy.orbich@gmail.com>
2022-04-08 09:18:39 -0400

    Added Amethyst Blocks

--- src/main/java/net/tomlorbich/tutorialmod/TutorialMod.java
+++ src/main/java/net/tomlorbich/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.tomlorbich.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.tomlorbich.tutorialmod.block.ModBlocks;
 import net.tomlorbich.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	public void onInitialize() {
 
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
